edeac4b2d1c961fe39ddae040b488750c3c1ec51
Nathan Argetsinger <nargetdev@gmail.com>
2023-12-17 21:35:59 -0800

    using namespace std;

--- examples/stream-with-finality/driver.hpp
+++ examples/stream-with-finality/driver.hpp
@@ -5,20 +5,22 @@
 #include <vector>
 #include <deque>
 
+using namespace std;
+
 class driver {
 public:
     driver(int BUFFER_LEN = 4, int LOCAL_AGREEMENT_N = 2, int PROMPT_LEN = 100);
-    std::tuple<std::vector<std::string>, std::deque<std::vector<std::string>>, std::vector<std::string>> drive(const std::string& line);
+    tuple<vector<string>, deque<vector<string>>, vector<string>> drive(const string& line);
 
 private:
-    std::deque<std::vector<std::string>> ctxBuffer;
+    deque<vector<string>> ctxBuffer;
     int LOCAL_AGREEMENT_N;
     int PROMPT_LEN;
-    std::vector<std::string> committed_tokens;
+    vector<string> committed_tokens;
     bool DEBUG;
     int lines_read;
 
-    static std::vector<std::string> fnDriverStep(const std::vector<std::string>& prompt, const std::deque<std::vector<std::string>>& buffer, int LOCAL_AGREEMENT_N);
+    static vector<string> fnDriverStep(const vector<string>& prompt, const deque<vector<string>>& buffer, int LOCAL_AGREEMENT_N);
 };
 
 #endif // DRIVER_HPP
